4ee1e85d8c85a4c90089a8b2b2c4099cf43142c7
jt2jt2 <azusaorenoyome1111@gmail.com>
2019-08-21 05:23:41 +0900

    signup and signin

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resource :registrations, only: [:new, :create]
+  
+  resource :sessions, only: [:new, :create, :destroy]
+  root to: 'registrations#new'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
