9e3a77a30baa8c9453f74bc5b5b271313ad5884f
jt2jt2 <azusaorenoyome1111@gmail.com>
2019-09-23 07:20:55 +0900

    signin and signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resource :settings, only: [:edit, :update]
   resource :registrations, only: [:new, :create]
-  
+  resources :users, only: [:index, :show]
   resource :sessions, only: [:new, :create, :destroy]
+  
   root to: 'registrations#new'
 	resource :registrations, only: [:new, :create]
 	resource :sessions, only: [:new, :create, :destroy]
